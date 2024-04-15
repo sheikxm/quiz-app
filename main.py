ee4a9b186d71c711496e250b63f23fca3e05177e
mohamed sheik <114642874+sheikxm@users.noreply.github.com>
2024-04-15 12:38:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import json
+
+
+def run():
+    st.set_page_config(
+        page_title="Cicada",
+        page_icon="❓",
+    )
+
+
+if __name__ == "__main__":
+    run()
+
+# Custom CSS for the buttons
+st.markdown("""
+<style>
+div.stButton > button:first-child {
+    display: block;
+    margin: 0 auto;
+</style>
+""", unsafe_allow_html=True)
+
+# Initialize session variables if they do not exist
+default_values = {'current_index': 0, 'current_question': 0,
+                  'score': 0, 'selected_option': None, 'answer_submitted': False}
+for key, value in default_values.items():
+    st.session_state.setdefault(key, value)
+
+# Load quiz data
+with open('content/quiz_data.json', 'r', encoding='utf-8') as f:
+    quiz_data = json.load(f)
+
+
+def restart_quiz():
+    st.session_state.current_index = 0
+    st.session_state.score = 0
+    st.session_state.selected_option = None
+    st.session_state.answer_submitted = False
+
+
+def submit_answer():
+
+    # Check if an option has been selected
+    if st.session_state.selected_option is not None:
+        # Mark the answer as submitted
+        st.session_state.answer_submitted = True
+        # Check if the selected option is correct
+        if st.session_state.selected_option == quiz_data[st.session_state.current_index]['answer']:
+            st.session_state.score += 10
+    else:
+        # If no option selected, show a message and do not mark as submitted
+        st.warning("Please select an option before submitting.")
+
+
+def next_question():
+    st.session_state.current_index += 1
+    st.session_state.selected_option = None
+    st.session_state.answer_submitted = False
+
+
+# Title and description
+st.title("Streamlit Quiz App")
+
+# Progress bar
+progress_bar_value = (st.session_state.current_index + 1) / len(quiz_data)
+st.metric(label="Score",
+          value=f"{st.session_state.score} / {len(quiz_data) * 10}")
+st.progress(progress_bar_value)
+
+# Display the question and answer options
+question_item = quiz_data[st.session_state.current_index]
+st.subheader(f"Question {st.session_state.current_index + 1}")
+st.title(f"{question_item['question']}")
+
+
+st.markdown(""" ___""")
+
+# Answer selection
+options = question_item['options']
+correct_answer = question_item['answer']
+
+if st.session_state.answer_submitted:
+    for i, option in enumerate(options):
+        label = option
+        if option == correct_answer:
+            st.success(f"{label} (Correct answer)")
+        elif option == st.session_state.selected_option:
+            st.error(f"{label} (Incorrect answer)")
+        else:
+            st.write(label)
+else:
+    for i, option in enumerate(options):
+        if st.button(option, key=i, use_container_width=True):
+            st.session_state.selected_option = option
+
+st.markdown(""" ___""")
+
+# Submission button and response logic
+if st.session_state.answer_submitted:
+    if st.session_state.current_index < len(quiz_data) - 1:
+        st.button('Next', on_click=next_question)
+    else:
+        score_percentage = (st.session_state.score /
+                            (len(quiz_data) * 10)) * 100
+        st.write(
+            f"Quiz completed! Your score is: {st.session_state.score} / {len(quiz_data) * 10} ({score_percentage}%)")
+        if st.session_state.score >= 70:
+            st.write(
+                "Congratulations! You scored 70 or above. Here's your key: XYZ123")
+        if st.button('Restart', on_click=restart_quiz):
+            pass
+else:
+    if st.session_state.current_index < len(quiz_data):
+        st.button('Submit', on_click=submit_answer)
